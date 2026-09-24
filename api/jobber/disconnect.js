d4a44802010a420d0c92eafa77782dc4549543f8
jm8651943-del <jm8651943@gmail.com>
2026-09-24 15:25:16 -0700

    Wire integration path: api/jobber/disconnect.js

--- api/jobber/disconnect.js
+++ api/jobber/disconnect.js
@@ -1,6 +1,8 @@
 const { clearJobberSession } = require("../../lib/session");
+const { applyCors } = require("../../lib/cors");
 
 module.exports = async function handler(req, res) {
+  if (applyCors(req, res)) return;
   if (req.method !== "POST") return res.status(405).json({ error: "Method not allowed" });
   clearJobberSession(res);
   return res.status(200).json({ connected: false });
